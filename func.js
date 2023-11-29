4ee11602f969194fb5692943d98153524b633bf5
Mfaizan103 <muhammadfaizanbhatti103@gmail.com>
2023-11-29 15:47:30 +0500

    fourth commit

---
+++ func.js
@@ -0,0 +1,9 @@
+// JavaScript function to navigate to the next page
+function nextPage () {
+    // You can replace 'nextPage.html' with the actual URL of your next page
+    window.location.href = 'report_missing_lost_person_2.html';
+};
+function prevPage () {
+    // You can replace 'nextPage.html' with the actual URL of your next page
+    window.location.href = 'report_missing_lost_person.html';
+};
